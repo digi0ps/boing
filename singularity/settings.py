cedec375b6d7dc44b1dd034e1be47ec0ca8f15fc
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-05-15 17:44:24 +0530

    Set Prepend to False and added static config

--- singularity/settings.py
+++ singularity/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*47q7sli(1g)v1-5ab9z+be+@8m#cu50z54(@_4t-46#z-0_@0'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-PREPEND_WWW = True
+PREPEND_WWW = False
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
