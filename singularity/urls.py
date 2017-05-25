d279b0e236ee14fdbfe1641d56c0b46363447a57
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-05-25 14:37:55 +0530

    Hardcoded the urls for favicons

--- singularity/urls.py
+++ singularity/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import url
 from django.contrib import admin
 from rainbow import views
+from singularity import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
     url(r'^fluff/$', views.fluff, name='fluff'),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += staticfiles_urlpatterns()
