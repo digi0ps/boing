cf42cecaf0927017e087330613250e2d7600bbf2
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-06-04 01:36:11 +0530

    Made small tweaks

--- singularity/settings.py
+++ singularity/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*47q7sli(1g)v1-5ab9z+be+@8m#cu50z54(@_4t-46#z-0_@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 PREPEND_WWW = False
